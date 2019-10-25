5841c791d61102c3a6cb364664950b6d8dfa46c9
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-10-25 12:02:44 +0530

    a

--- client.js
+++ client.js
@@ -1,31 +1,6 @@
 var express = require('express');
 var app = express();
 var WebSocket = require('ws');
-var http = require('http');
-const hostname = 'ec2-54-184-89-72.us-west-2.compute.amazonaws.com';
-const port = 8080;
-
-
-
-var exec = require('child_process').exec,
-    publicIp = '54.184.89.72',
-    keyPath = '~/Downloads/aws_instance.pem',
-    portForwarding = function() {
-        var portVal = document.querySelector('#portVal').value,
-            poertForwardingCmd = 'ssh -L ' + portVal + ':' + publicIp + ':22 ubuntu@' + publicIp + ' -i ' + keyPath;
-        console.log(portVal);
-        console.log(poertForwardingCmd);
-        exec(getCommand,
-            function(error, stdout, stderr) {
-                console.log('PF:Executing');
-                if (error !== null) {
-                    console.log('exec error: ' + error);
-                }
-            }
-        );
-        console.log('Sending' + portVal);
-    };
-
 
 //web socket connection
 const connection = new WebSocket('ws://ec2-54-184-89-72.us-west-2.compute.amazonaws.com:8080');
@@ -42,11 +17,3 @@ connection.onclose = () => {
 connection.onerror = (error) => {
     console.error('failed to connect', error);
 }
-
-app.post('/clicked', (req, res) => {
-    const click = { clickTime: new Date() };
-    console.log(click);
-
-    res.sendStatus(201);
-
-});
--- server.js
+++ server.js
@@ -4,9 +4,5 @@ const WebSocket = require('ws');
 const webSocketServer = new WebSocket.Server({ port: 8080 });
 
 webSocketServer.on('connection', (webSocket) => {
-    webSocket.on('message', (message) => {
-        console.log('Received:', message);
-        console.log('Hi from server')
-
-    });
+    webSocketServer.sendMessage("Welcome to the server!");
 });
