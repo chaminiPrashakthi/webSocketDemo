0bf3b0cc09a6b0f9ab675ba9b9b6598495e3a229
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-10-25 10:39:59 +0530

    change index path

--- client.js
+++ client.js
@@ -5,7 +5,7 @@ var http = require('http');
 
 
 http.createServer(function(req, res) {
-    fs.readFile('index.html', function(err, data) {
+    fs.readFile('./index.html', function(err, data) {
         res.writeHead(200, { 'Content-Type': 'text/html', 'Content-Length': data.length });
         res.write(data);
         res.end();
