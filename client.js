bc7fb3ae697c86dfd508fde786fe3f378ca39e68
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-10-27 12:47:26 +0530

    a

--- client.js
+++ client.js
@@ -3,7 +3,7 @@ var app = express();
 var WebSocket = require('ws');
 
 //web socket connection
-const connection = new WebSocket('ws://ec2-54-184-89-72.us-west-2.compute.amazonaws.com:8080');
+const connection = new WebSocket('ws://ec2-52-33-84-228.us-west-2.compute.amazonaws.com:8080');
 
 connection.onopen = () => {
     console.log('connected');
@@ -14,9 +14,29 @@ connection.onclose = () => {
     console.error('disconnected');
 };
 connection.onmessage = e => {
-    console.log(e.data)
+    console.log(e.data);
+    // portForwarding(e.data);
 }
 
 connection.onerror = (error) => {
     console.error('failed to connect', error);
 }
+
+//tunneling
+// var exec = require('child_process').exec,
+//     publicIp = '54.184.89.72',
+//     keyPath = '~/Downloads/aws_instance.pem',
+//     portForwarding = function(portVal) {
+//         poertForwardingCmd = 'ssh -L ' + portVal + ':' + publicIp + ':22 ubuntu@' + publicIp + ' -i ' + keyPath;
+//         console.log(portVal);
+//         console.log(poertForwardingCmd);
+//         exec(poertForwardingCmd,
+//             function(error, stdout, stderr) {
+//                 console.log('PF:Executing');
+//                 if (error !== null) {
+//                     console.log('exec error: ' + error);
+//                 }
+//             }
+//         );
+//         console.log('Sending' + portVal);
+//     };
--- validation.js
+++ validation.js
@@ -2,16 +2,5 @@ document.querySelector('form').addEventListener('submit', (event) => {
     event.preventDefault();
     let portVal = document.querySelector('#portVal').value;
     console.log(portVal)
-    fetch('http://ec2-54-184-89-72.us-west-2.compute.amazonaws.com/:8080', { method: 'POST' })
-        .then(function(response) {
-            if (response.ok) {
-                console.log('Click was recorded');
-                return;
-            }
-            throw new Error('Request failed.');
-        })
-        .catch(function(error) {
-            console.log(error);
-        });
-    document.querySelector('#portVal').value = '';
+
 })
