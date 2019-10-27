18ec33a13fe860f8f6b4be7732965a6dd87fd4a2
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-10-28 01:55:16 +0530

    l

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var bodyParser = require('body-parser');
 var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 // Running Server Details.
-var server = app.listen(53, function() {
+var server = app.listen(8000, function() {
     var host = server.address().address
     var port = server.address().port
     console.log("listening at Port", host, port)
