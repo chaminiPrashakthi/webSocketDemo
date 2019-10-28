79afcde79cf2dd5fe19cae6e3901229ea0e05e17
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-10-28 16:21:40 +0530

    k

--- server.js
+++ server.js
@@ -18,11 +18,13 @@ app.get('/', function(req, res) {
 
 app.post('/connection', function(req, res) {
     portVal = req.body.portVal;
-    var data = {
-        "portVal": portVal,
-    }
-    console.log("DATA is " + JSON.stringify(data));
-    ws.send(JSON.stringify(data));
+    wss.on('connection', ws => {
+        ws.on('message', message => {
+            console.log(`Received message => ${message}`)
+        })
+        ws.send(portVal);
+        console.log('ssh with ' + portVal);
+    })
     return res.redirect('/');
 });
 
