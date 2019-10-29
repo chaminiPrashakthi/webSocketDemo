cae233fb7c29b37d3407dedca86dc4659f329e21
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-10-29 08:00:08 +0530

    k

--- server.js
+++ server.js
@@ -3,14 +3,10 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = require('path');
-const bodyParser = require('body-parser');
 
 app.use('/', router);
 app.listen(process.env.port || 8000);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+
 
 var portVal = null;
 var text;
