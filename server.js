64cba0b9ce33c151a665763344be7bb12210d491
Chamini Jayasinghe <Chamini.Perera@syscolabs.com>
2019-10-29 07:45:46 +0530

    k

--- server.js
+++ server.js
@@ -3,12 +3,8 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const path = require('path');
-const bodyParser = require('body-parser');
 
 app.use('/', router);
-app.listen(process.env.port || 8000);
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
 
 var portVal = null;
 var text;
@@ -22,14 +18,14 @@ wss.on('connection', function(connection) {
         res.sendFile(path.join(__dirname + '/index.html'));
     });
 
-    app.post('/', function(req, res) {
+    app.post('/connection', function(req, res) {
         portVal = req.body.portVal;
         console.log('ssh with ' + portVal);
         // Send data back to the client
         connection.send(portVal);
         // data is received from client
         connection.on('message', function(message) {
-            if (message.equals('Error')) {
+            if (message != 'Error') {
                 console.log('Success')
                 text = 'Success'
             } else {
